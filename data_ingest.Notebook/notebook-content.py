ce96c1f68d93a21d790d18845ac3dda29c0c1884
Lanre Adetola <r0913836@student.thomasmore.be>
2026-08-13 07:47:19 -0700

    Committing 3 items from workspace d78dd0c7-584e-417f-8893-4f4dd090b375

---
+++ data_ingest.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "60548a93-566e-4621-93fd-71406dc7ae2a",
+# META       "default_lakehouse_name": "movies_lh",
+# META       "default_lakehouse_workspace_id": "d78dd0c7-584e-417f-8893-4f4dd090b375",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "60548a93-566e-4621-93fd-71406dc7ae2a"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
